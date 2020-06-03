7b8438ea9e26cc8ad38029acabd6f02e8b6f270f
Matt <matthew.bean@jessup.edu>
2020-06-03 12:43:06 -0700

    fixed server issue

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,7 +16,7 @@ export default class Home extends Component {
   search = async (search, type) => {
     search = search.toLowerCase();
     try {
-      let res = await axios.get(`http://www.dnd5eapi.co${search}`);
+      let res = await axios.get(`https://www.dnd5eapi.co${search}`);
       //fetch new results and set loading to false
       this.setState({
         ...this.state,
